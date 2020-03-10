b64209b9ee8be56d1e1040553e885ae75809f7e4
Damir Gimatdinov <dgimatdinov@yandex.ru>
2020-03-11 00:03:36 +0300

    Modify checker

--- ReservedIdentifierChecker.cpp
+++ ReservedIdentifierChecker.cpp
@@ -7,11 +7,19 @@
 #include "clang/AST/Decl.h"
 #include "clang/AST/DeclBase.h"
 
+#include <fstream>
 #include <string>
 #include <regex>
 
+#include "llvm/Support/JSON.h"
 using namespace clang;
 using namespace ento;
+using namespace std;
+
+static ifstream input("/Users/misterd/PycharmProjects/web_scraping/personal.json");
+//static StringRef pathToHeaderJSON("/Users/misterd/PycharmProjects/web_scraping/personal.json");
+//static llvm::SourceMgr sm;
+//static llvm::yaml::Stream stream(pathToHeaderJSON, sm);
 
 namespace {
 class DeclVisitor : public clang::RecursiveASTVisitor<DeclVisitor> {
@@ -39,15 +47,37 @@ private:
 
 // Declaration visitor methods
 bool DeclVisitor::VisitFunctionDecl(const FunctionDecl *FD) {
-  SourceManager &sm= MGR.getSourceManager();
+  /*SourceManager &sm= MGR.getSourceManager();
   if (!sm.isInMainFile(FD->getLocation()))
       return true;
   FileID mainFileID = sm.getMainFileID();
   for (auto it = sm.fileinfo_begin(); it != sm.fileinfo_end(); it++) {
       SourceLocation includeLoc = sm.getIncludeLoc(sm.translateFile(it->first));
       if (includeLoc.isValid() && sm.isInFileID(includeLoc, mainFileID))
-          reportBug(it->first->getName(), FD);
-  }
+          //reportBug(it->first->getName(), FD);
+  }*/
+  //StringRef pathToHeaderJSON("/Users/misterd/PycharmProjects/web_scraping/personal.json");
+  //StringRef pathToYaml("/Users/misterd/Desktop/coding/checkers/example2.yaml");
+  //ifstream input("/Users/misterd/Desktop/coding/checkers/empthy.json");
+
+  //llvm::SourceMgr sm;
+  //llvm::yaml::Stream stream(pathToHeaderJSON, sm);
+  //llvm::yaml::Stream stream(pathToYaml, sm);
+
+  //for (auto doc : theDocList)
+    //  reportBug(doc, FD);
+   
+  /*for (llvm::yaml::document_iterator di = stream.begin(), de = stream.end();
+          di != de; ++di) {
+      llvm::yaml::Node *n = di->getRoot();
+      //llvm::yaml::ScalarNode *n = dyn_cast<llvm::yaml::ScalarNode>(di->getRoot());
+      if (n) {
+        // Do something with n...
+        reportBug(std::to_string(n->getRawTag().size()), FD);
+        //reportBug(n->getAnchor().size(), FD);
+      } else
+        break;
+ }*/
 
   return true;
 }
